1e5272ffb9a836898bb3c01fbb36988c032e6b4c
RohitKumar0999 <146411404+RohitKumar0999@users.noreply.github.com>
2026-07-23 16:14:03 +0530

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstringBruteForce(s) {
+        let res = 0;
+        for (let l = 0; l < s.length; l++) {
+            let mySet = new Set();
+            for (let r = l; r < s.length; r++) {
+                if (mySet.has(s[r])) {
+                    break;
+                }
+                mySet.add(s[r]);
+            }
+            res = Math.max(res, mySet.size);
+        }
+        return res;
+    }
+
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        return this.lengthOfLongestSubstringBruteForce(s);
+        let l = 0;
+        let mySet = new Set();
+        let res = 0;
+
+        for (let r = 0; r < s.length; r++) {
+            while (mySet.has(s[r])) {
+                mySet.delete(s[l]);
+                l++;
+            }
+            mySet.add(s[r]);
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
