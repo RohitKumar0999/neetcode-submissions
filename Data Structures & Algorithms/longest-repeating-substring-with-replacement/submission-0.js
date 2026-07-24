db3c38b9a4a14416ce3525dd0d6f522e2d375e9c
RohitKumar0999 <146411404+RohitKumar0999@users.noreply.github.com>
2026-07-24 14:18:25 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacementBruteForce(s, k) {
+        let res = 0;
+        for (let i = 0; i < s.length; i++) {
+            let count = new Map();
+            let maxFreq = 0;
+
+            for (let j = i; j < s.length; j++) {
+                count.set(s[j], (count.get(s[j]) || 0) + 1);
+                maxFreq = Math.max(maxFreq, count.get(s[j]));
+
+                if (j - i + 1 - maxFreq <= k) {
+                    res = Math.max(res, j - i + 1);
+                }
+            }
+        }
+        return res;
+    }
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        return this.characterReplacementBruteForce(s, k);
+    }
+}
